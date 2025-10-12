d495fe82f62f941629a1825f0069823ca90fe337
Manvithtech07 <154258049+Manvithtech07@users.noreply.github.com>
2025-10-12 08:38:21 +0530

    Add files via upload

---
+++ 02-Arrays/TwoSum.c
@@ -0,0 +1,24 @@
+//case1 -Input: nums = [2,7,11,15], target = 9 Output: [0,1]
+#include<stdio.h>
+void TwoSum(int arr[],int size,int target){
+    int found = 0;
+    for(int i=0;i<size;i++){
+        for(int j=i+1;j<size;j++){
+            if(arr[i]+arr[j]==target){
+                printf("%d,%d",i,j);
+                found = 1;
+                return;
+            }
+        }
+    }
+    if(found == 0){
+        printf("No two elements sum up to the target.\n");
+    }
+}
+int main(){
+    int arr[]={2,6,11,15};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    int target = 9;
+    TwoSum(arr,size,target);
+    return 0;
+}
